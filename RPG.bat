e3c8a0dcc21da14edfa6a37588b82696c11c0813
TealDR <100844080+TealDR@users.noreply.github.com>
2022-03-06 17:22:05 +1030

    Update RPG.bat

--- RPG.bat
+++ RPG.bat
@@ -5,6 +5,13 @@ echo RULES
 echo ----------------------
 echo This game is property of ME
 echo Please do not claim this as yours!
+set /a money=0
+set /a level=1
+set /a xp=0
+set /a xprequired=100
+set /a maxhp=100
+set /a hp=100
+set /a location=tutorial
 
 :settings1
 Pause
@@ -16,7 +23,7 @@ echo Type 2 for Light Mode
 set /p Input=
 if %input%==1 goto darkmode
 if %input%==2 goto lightmode
-goto settings2
+goto settings1
 
 :darkmode
 COLOR 07
@@ -33,27 +40,59 @@ echo ----------------------
 echo Type 1 to choose Save Slot 1
 echo Type 2 to choose Save Slot 2
 set /p input=
-if %input%==1 goto load1
-if %input%==2 goto load2
+if %input%==1 set /a save=1
+if %input%==2 set /a save=2
+goto load
 
-:load1
+:load
 (
 set /a money=
 set /a level=
-)<RPG1.dlb
-set /a Save=1
+set /a xp=
+set /a xprequired=
+set /a maxhp=
+set /a hp=
+set /a location=
+)<RPG%save%.dlb
+goto %location%
 
-:load2
-(
-set /a money=
-set /a level=
-)<RPG2.dlb
-set /a Save=2
 
 :save
-if %save%=1 goto save1
-if %save%=2 goto save2
+(
+echo %money%
+echo %level%
+echo %xp%
+echo %xprequired%
+echo %maxhp%
+echo %hp%
+echo %location%
+)>RPG%save%.dlb
+goto menu
 
-:save1
 
-:save2
+:tutorial
+Pause
+cls
+echo TUTORIAL
+echo ----------------------
+echo Welcome to the world of Ghantifell.
+echo This tutorial will tell you how to operate the game.
+Pause
+cls
+echo TUTORIAL
+echo ----------------------
+echo Below this text is the menu.
+echo On the top, there is your Money and Level. Money can be used to purchase items. Money is obtained through battling. Your level quantifies your strength and HP.
+echo On the left, it has the input required to do the action. On the right, there is the action
+
+:menu
+echo Hp: %hp% / %maxhp%       Level: %level%       XP: %xp% / %xprequired%
+echo Battle | Go into the battle menu
+echo Rest   | Resting heals you to max HP.
+echo Save   | Save current progress
+set /p input= 
+if %input%==Battle goto battle
+if %input%==Rest set /a hp=%maxhp%
+if %input%==Save goto save
+goto menu
+
